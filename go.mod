93ad81114bc382ab6c867b47cbaeab3e70eabf86
ucwong <ucwong@126.com>
2021-02-07 11:35:31 +0800

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ucwong/sign
+
+go 1.15
+
+require github.com/CortexFoundation/CortexTheseus v1.10.19
---
+++ sign.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"hash"
+	"log"
+	"time"
+
+	"github.com/CortexFoundation/CortexTheseus/common"
+	"github.com/CortexFoundation/CortexTheseus/common/hexutil"
+	"github.com/CortexFoundation/CortexTheseus/crypto"
+)
+
+type KeccakState interface {
+	hash.Hash
+	Read([]byte) (int, error)
+}
+
+var (
+	//secp256k1N, _ = new(big.Int).SetString("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
+	testpri = "289c2857d4598e37fb9647507e47a309d6133539bf21a8b9cb6df88fd5232032"
+)
+
+const (
+//DigestLength = 32
+)
+
+func main() {
+	msg := "hello"
+	sig, err := SignData(msg, testpri)
+	if err != nil {
+		log.Println(err)
+	}
+	suc := Verify(msg, "0x970E8128AB834E8EAC17Ab8E3812F010678CF791", hexutil.Encode(sig[:]), time.Now().Unix())
+	if suc {
+		log.Println("Sign suc")
+	}
+}
+
+func Verify(msg, addr, sig string, timestamp int64) bool {
+	if len(msg) == 0 || len(addr) == 0 || len(sig) == 0 || timestamp == 0 {
+		log.Println("params failed msg:" + msg + ", addr:" + addr + ", sig:" + sig)
+		return false
+	}
+
+	if time.Now().Unix()-int64(30) > timestamp {
+		return false
+	}
+
+	if time.Now().Unix()+int64(30) < timestamp {
+		return false
+	}
+
+	//sig_, _ := SignData(msg, testpri)
+	//log.Printf("[signature] : want:%s, have:%s", hexutil.Encode(sig_[:]), sig)
+	s := hexutil.MustDecode(sig)
+
+	if len(s) == 0 {
+		log.Println("Signature failed m")
+		return false
+	}
+	recoveredAddr, err := EcRecover([]byte(msg), s)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	if common.HexToAddress(addr) != recoveredAddr {
+		log.Printf("Address mismatch: want: %v have: %v\n", addr, recoveredAddr.Hex())
+		return false
+	}
+
+	return true
+}
+
+func EcRecover(data, sig hexutil.Bytes) (common.Address, error) {
+	if len(sig) != 65 {
+		return common.Address{}, fmt.Errorf("signature must be 65 bytes long")
+	}
+	if sig[64] != 27 && sig[64] != 28 {
+		return common.Address{}, fmt.Errorf("invalid Cortex signature (V is not 27 or 28) %v", sig[64])
+	}
+	sig[64] -= 27 // Transform yellow paper V from 27/28 to 0/1
+	hash, _ := SignHash(data)
+	rpk, err := crypto.SigToPub(hash, sig)
+	if err != nil {
+		return common.Address{}, err
+	}
+	return crypto.PubkeyToAddress(*rpk), nil
+}
+
+func SignHash(data []byte) ([]byte, string) {
+	msg := fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(data), data)
+	return crypto.Keccak256([]byte(msg)), msg
+}
+
+func SignData(msg string, pri string) (sig []byte, err error) {
+	k0, _ := crypto.HexToECDSA(pri)
+	msg0, _ := SignHash([]byte(msg)) //Keccak256([]byte(msg))
+	sig, err = crypto.Sign(msg0, k0)
+	sig[64] += 27
+	return
+}
